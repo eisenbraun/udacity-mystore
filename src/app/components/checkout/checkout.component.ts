dd36491fd38a341fecd69e61113519bf1b727cc4
Michael Eisenbraun <3506550+eisenbraun@users.noreply.github.com>
2021-09-25 06:00:24 -0400

    Adds form handler and validation

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CartService } from 'src/app/services/cart.service';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
+  fullName: string = ''
+  creditCard: string = ''
+  address: string = ''
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
+
+  submitOrder() {
+    this.cartService.clearCart()
+    
+  }
 
   ngOnInit(): void {
   }
